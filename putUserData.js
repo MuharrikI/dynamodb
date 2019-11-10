4281e3873b6cbeee5c52bea1d88ff0e4baf450bc
MuharrikI <pracore97@gmail.com>
2019-11-10 16:00:17 +0700

    putUserData added

---
+++ putUserData.js
@@ -0,0 +1,26 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: "us-east-1" });
+
+exports.handler = async (event, context) => {
+    const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+
+    const params = {
+        TableName: "tes",
+        Key: {
+            id: "12345",
+            firstname: "dana",
+            lastname: "white"
+        }
+    }
+
+    try {
+        const data = await documentClient.put(params).promise();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
