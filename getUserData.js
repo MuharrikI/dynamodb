70851631ed9650454011b69146c2ce0ffb154f34
MuharrikI <pracore97@gmail.com>
2019-11-10 16:23:16 +0700

    responsbody

--- getUserData.js
+++ getUserData.js
@@ -7,18 +7,27 @@ exports.handler = async (event, context) => {
     const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
     const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
 
+
+    let responsBody = "";
+    let statusCode = 0;
+
+
+    const { id } = event.pathParameters;
+
     const params = {
         TableName: "tes",
         Key: {
-            id: "12345"
+            id: id
         }
     }
 
     try {
         const data = await documentClient.get(params).promise();
-        console.log(data);
+        responsBody = JSON.stringify(data.Item);
+        statusCode = 200;
     } catch (err) {
-        console.log(err);
+        responsBody = "Unable to process";
+        statusCode = 403;
     }
 
 }
