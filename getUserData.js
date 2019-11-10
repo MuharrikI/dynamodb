ffd4c4c0b2c72c9ffe15168f10ce79e5eafefc90
MuharrikI <pracore97@gmail.com>
2019-11-10 15:49:42 +0700

    async await

--- getUserData.js
+++ getUserData.js
@@ -1,23 +1,24 @@
 'use strict'
 const AWS = require('aws-sdk');
 
-AWS.config.update({region:"us-east-1"});
+AWS.config.update({ region: "us-east-1" });
 
-exports.handler = function (event, context, callback) {
-    const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region:"us-east-1"});
+exports.handler = async (event, context) => {
+    const ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
 
     const params = {
         TableName: "tes",
         Key: {
-            id : "12345"
+            id: "12345"
         }
     }
 
-    documentClient.get(params, (err, data) => {
-        if (err){
-            console.log(err);
-        }
+    try {
+        const data = await documentClient.get(params).promise();
         console.log(data);
-    })
+    } catch (err) {
+        console.log(err);
+    }
+
 }
